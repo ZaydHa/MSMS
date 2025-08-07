2159446f5d29e5b77ea610035075781d26e47bea
Zayd Hamza <zayd@zayds-mbp.home>
2025-08-07 19:53:28 +1000

    Stage 3

--- MSMS.py
+++ MSMS.py
@@ -23,4 +23,32 @@ def add_student(name):
     next_student_id += 1 # Increment student ID for next student
     return student
 
+def add_teacher(name, speciality):
+    global next_teacher_id
+    teacher = teacher(next_teacher_id, name, speciality) # Create new teacher object
+    teacher_db.append(teacher) # Add to teacher database
+    next_teacher_id += 1 # Increment teacher ID for next teacher
+    return teacher
 
+def find_student_by_id(student_id):
+    for student in student_db:
+        if student.id == student_id:
+            return student
+    return None
+
+def find_teacher_by_id(teacher_id):
+    for teacher in teacher_db:
+        if teacher.id == teacher_id:
+            return teacher
+    return None
+
+def print_all_students():
+    print ("All Students:")
+    for student in student_db:
+        print(f"ID: {student.id}, Name: {student.name}, Enrolled in: {', '.join(student.enrolled_in)}")
+
+def print_all_teachers():
+    print("All Teachers:")
+    for teacher in teacher_db:
+        print(f"ID: {teacher.id}, Name: {teacher.name}, Speciality: {teacher.speciality}")
+        
