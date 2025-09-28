de4d1bd755c07f7770b74bcb3febd714b568f7b6
Zayd Hamza <zayd@zayds-mbp.home>
2025-09-28 12:06:30 +1000

    chore(gui): use PST4 manager adapter

--- gui/main_dashboard4.py
+++ gui/main_dashboard4.py
@@ -2,6 +2,7 @@ import streamlit as st
 from app.schedule import ScheduleManager
 from gui.student_pages4 import show_student_management_page
 from gui.roster_pages4 import show_roster_page
+from app.schedule import ScheduleManager
 
 def launch():
     st.set_page_config(layout="wide", page_title="MSMS Dashboard")
